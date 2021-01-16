9f8a3cae2e00db4afbd17e3fafacbe80ee565eeb
klosowski4work <piotr.klosowski4w@gmail.com>
2021-01-16 16:56:24 +0100

    added iwa prefix for components and directives added .prettierrc file with configuration for prettier plugin added viewport component

---
+++ src/app/components/viewport/viewport.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'iwa-viewport',
+  templateUrl: './viewport.component.html',
+  styleUrls: ['./viewport.component.css']
+})
+export class ViewportComponent implements OnInit {
+
+  constructor() { }
+
+
+  ngOnInit(): void {
+  }
+
+}
